1def957380c1e8870e70e1a82ead43df4635a269
pavel <ptashinsky@tut.by>
2020-01-14 07:05:36 +0300

    second

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import gendiff from '../src';
 
 test('gendiff', () => {
   expect(gendiff('/home/ebces/project-lvl2-s487/__tests__/__fixtures__/before.json',
-   '/home/ebces/project-lvl2-s487/__tests__/__fixtures__/after.json')).toEqual({
+    '/home/ebces/project-lvl2-s487/__tests__/__fixtures__/after.json')).toEqual({
     ' host': 'hexlet.io',
     '-timeout': 50,
     '+timeout': 20,
