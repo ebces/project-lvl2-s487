2daac7525f2d7e6cc5da92294d5064c5defa5ed9
pavel <ptashinsky@tut.by>
2020-02-11 08:34:04 +0300

    sixth

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -19,11 +19,14 @@ const plainFormat = (firstElem, secondElem) => {
           : [...acc, `Property "${newPath.join(' ')}${elem}" was added with value: ${sFile[elem]}`];
       }
       if (_.has(fFile, elem) && _.has(sFile, elem)) {
-        if (!_.isEqual(fFile[elem], sFile[elem])) {
-          console.log(newPath);
-          return typeof sFile[elem] === 'object'
-            ? [...acc, ...iter(fFile[elem], sFile[elem], newPath)] // [...acc, `Property "${newPath.join(' ')}${elem}" was update. From "${fFile[elem]}" to [complex value]`]
-            : [...acc, `Property "${newPath.join(' ')}${elem}" was update. From "${fFile[elem]}" to "${sFile[elem]}"`];
+        if (!_.isEqual(fFile[elem], sFile[elem]) && typeof fFile[elem] !== 'object' && typeof sFile[elem] !== 'object') {
+          return [...acc, `Property "${newPath.join(' ')}${elem}" was updated. From ${fFile[elem]} to ${sFile[elem]}`];
+        }
+        if (!_.isEqual(fFile[elem], sFile[elem]) && typeof fFile[elem] !== 'object' && typeof sFile[elem] === 'object') {
+          return [...acc, `Property "${newPath.join(' ')}${elem}" was updated. From ${fFile[elem]} to [complex value]`];
+        }
+        if (!_.isEqual(fFile[elem], sFile[elem]) && typeof fFile[elem] === 'object' && typeof sFile[elem] !== 'object') {
+          return [...acc, `Property "${newPath.join(' ')}${elem}" was updated. From [complex value] to ${sFile[elem]}`];
         }
       }
 
