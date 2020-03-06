c38bc4015417cca036e2e1fc0149e5512052fef1
ebces <ptashinsky@tut.by>
2020-03-06 20:10:46 +0300

    correct

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,37 +1,27 @@
 import fs from 'fs';
+import path from 'path';
 import gendiff from '../src';
 
 
-const getFileContent = (path) => fs.readFileSync(`${__dirname}${path}`, 'utf-8');
-
-const resultTree = getFileContent('/__fixtures__/resultTree');
-const resultString = getFileContent('/__fixtures__/resultString');
-const resultJSON = getFileContent('/__fixtures__/resultJSON');
-const resultYmlJSON = getFileContent('/__fixtures__/resultYML');
-
-const gendiffTestJSON = gendiff(`${__dirname}/__fixtures__/treeBefore.json`, `${__dirname}/__fixtures__/treeAfter.json`);
-const gendiffTestYML = gendiff(`${__dirname}/__fixtures__/treeBefore.yml`, `${__dirname}/__fixtures__/treeAfter.yml`);
-const gendiffTestINI = gendiff(`${__dirname}/__fixtures__/treeBefore.ini`, `${__dirname}/__fixtures__/treeAfter.ini`);
-
-const gendiffTestPlainJSON = gendiff(`${__dirname}/__fixtures__/treeBefore.json`, `${__dirname}/__fixtures__/treeAfter.json`, 'plain');
-const gendiffTestPlainYML = gendiff(`${__dirname}/__fixtures__/treeBefore.yml`, `${__dirname}/__fixtures__/treeAfter.yml`, 'plain');
-const gendiffTestPlainINI = gendiff(`${__dirname}/__fixtures__/treeBefore.ini`, `${__dirname}/__fixtures__/treeAfter.ini`, 'plain');
-
-const gendiffTestJsonJSON = gendiff(`${__dirname}/__fixtures__/treeBefore.json`, `${__dirname}/__fixtures__/treeAfter.json`, 'json');
-const gendiffTestJsonYML = gendiff(`${__dirname}/__fixtures__/treeBefore.yml`, `${__dirname}/__fixtures__/treeAfter.yml`, 'json');
-const gendiffTestJsonINI = gendiff(`${__dirname}/__fixtures__/treeBefore.ini`, `${__dirname}/__fixtures__/treeAfter.ini`, 'json');
+const getFixturePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+const getFileContent = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
 
 
 test('gendiff', () => {
-  expect(gendiffTestJSON).toEqual(resultTree);
-  expect(gendiffTestYML).toEqual(resultTree);
-  expect(gendiffTestINI).toEqual(resultTree);
+  const resultTree = getFileContent('resultTree');
+  const resultString = getFileContent('resultString');
+  const resultJSON = getFileContent('resultJSON');
+  const resultYmlJSON = getFileContent('resultYML');
 
-  expect(gendiffTestPlainJSON).toEqual(resultString);
-  expect(gendiffTestPlainYML).toEqual(resultString);
-  expect(gendiffTestPlainINI).toEqual(resultString);
+  expect(gendiff(getFixturePath('treeBefore.json'), getFixturePath('treeAfter.json'))).toEqual(resultTree);
+  expect(gendiff(getFixturePath('treeBefore.yml'), getFixturePath('treeAfter.yml'))).toEqual(resultTree);
+  expect(gendiff(getFixturePath('treeBefore.ini'), getFixturePath('treeAfter.ini'))).toEqual(resultTree);
 
-  expect(gendiffTestJsonJSON).toEqual(resultJSON);
-  expect(gendiffTestJsonYML).toEqual(resultYmlJSON);
-  expect(gendiffTestJsonINI).toEqual(resultJSON);
+  expect(gendiff(getFixturePath('treeBefore.json'), getFixturePath('treeAfter.json'), 'plain')).toEqual(resultString);
+  expect(gendiff(getFixturePath('treeBefore.yml'), getFixturePath('treeAfter.yml'), 'plain')).toEqual(resultString);
+  expect(gendiff(getFixturePath('treeBefore.ini'), getFixturePath('treeAfter.ini'), 'plain')).toEqual(resultString);
+
+  expect(gendiff(getFixturePath('treeBefore.json'), getFixturePath('treeAfter.json'), 'json')).toEqual(resultJSON);
+  expect(gendiff(getFixturePath('treeBefore.yml'), getFixturePath('treeAfter.yml'), 'json')).toEqual(resultYmlJSON);
+  expect(gendiff(getFixturePath('treeBefore.ini'), getFixturePath('treeAfter.ini'), 'json')).toEqual(resultJSON);
 });
--- src/formatters/defaultFormat.js
+++ src/formatters/defaultFormat.js
@@ -1,17 +1,6 @@
 import _ from 'lodash';
 
 
-const objectToString = (object, spaces) => {
-  const values = Object.entries(object);
-  return values.reduce((acc, elem) => {
-    const [key, value] = elem;
-    if (typeof value !== 'object') {
-      return `${acc}${spaces}${key}: ${value}\n`;
-    }
-    return `${acc} ${objectToString(value)}\n`;
-  }, '');
-};
-
 const defaultFormat = (firstElem, secondElem, spaces = '') => {
   const newSpaces = `${spaces}    `;
   const keysFirstObject = Object.keys(firstElem);
@@ -28,20 +17,20 @@ const defaultFormat = (firstElem, secondElem, spaces = '') => {
         return `${acc}${newSpaces}${newName}: ${defaultFormat(firstElem[elem], secondElem[elem], newSpaces)}\n`;
       }
       if (typeof firstElem[elem] !== 'object' && typeof secondElem[elem] === 'object') {
-        return `${acc}${newSpaces}${minusName}: ${firstElem[elem]}\n${newSpaces}${plusName}: {\n${newSpaces}${objectToString(secondElem[elem], newSpaces)}${newSpaces}}\n`;
+        return `${acc}${newSpaces}${minusName}: ${firstElem[elem]}\n${newSpaces}${plusName}: ${defaultFormat({}, secondElem[elem], newSpaces)}\n`;
       }
       if (typeof firstElem[elem] === 'object' && typeof secondElem[elem] !== 'object') {
-        return `${acc}${newSpaces}${minusName}: {\n${newSpaces}${objectToString(firstElem[elem], newSpaces)}${newSpaces}}\n${newSpaces}${plusName}: ${secondElem[elem]}\n`;
+        return `${acc}${newSpaces}${minusName}: ${defaultFormat(firstElem[elem], {}, newSpaces)}\n${newSpaces}${plusName}: ${secondElem[elem]}\n`;
       }
       return _.isEqual(firstElem[elem], secondElem[elem])
         ? `${acc}${newSpaces}${newName}: ${firstElem[elem]}\n`
         : `${acc}${newSpaces}${minusName}: ${firstElem[elem]}\n${newSpaces}${plusName}: ${secondElem[elem]}\n`;
     }
     if (!_.has(firstElem, elem) && _.has(secondElem, elem)) {
-      return typeof secondElem[elem] !== 'object' ? `${acc}${newSpaces}${plusName}: ${secondElem[elem]}\n` : `${acc}${newSpaces}${plusName}: {\n${newSpaces}${objectToString(secondElem[elem], newSpaces)}${newSpaces}}\n`;
+      return typeof secondElem[elem] !== 'object' ? `${acc}${newSpaces}${plusName}: ${secondElem[elem]}\n` : `${acc}${newSpaces}${plusName}: ${defaultFormat({}, secondElem[elem], newSpaces)}\n`;
     }
     if (_.has(firstElem, elem) && !_.has(secondElem, elem)) {
-      return typeof firstElem[elem] !== 'object' ? `${acc}${newSpaces}${minusName}: ${firstElem[elem]}\n` : `${acc}${newSpaces}${minusName}: {\n${newSpaces}${objectToString(firstElem[elem], newSpaces)}${newSpaces}}\n`;
+      return typeof firstElem[elem] !== 'object' ? `${acc}${newSpaces}${minusName}: ${firstElem[elem]}\n` : `${acc}${newSpaces}${minusName}: ${defaultFormat(firstElem[elem], {}, newSpaces)}\n`;
     }
     return acc;
   }, '');
