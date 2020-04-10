7cfbb3168220a425234a4cbfca858dea84a9ad34
pavel <ptashinsky@tut.by>
2020-04-10 11:39:35 +0300

    correct

--- src/AST.js
+++ src/AST.js
@@ -19,7 +19,7 @@ const makeAST = (firstData, secondData) => {
     }
 
     if (!_.has(firstData, key)) {
-      return makeNode(key, 'added', undefined, secondData[key]);
+      return makeNode(key, 'added', null, secondData[key]);
     }
 
     if (_.isEqual(firstData[key], secondData[key])) {
@@ -27,7 +27,7 @@ const makeAST = (firstData, secondData) => {
     }
 
     if (typeof firstData[key] === 'object' && typeof secondData[key] === 'object') {
-      return makeNode(key, 'hasChildren', undefined, undefined, makeAST(firstData[key], secondData[key]));
+      return makeNode(key, 'hasChildren', null, null, makeAST(firstData[key], secondData[key]));
     }
 
     return makeNode(key, 'changed', firstData[key], secondData[key]);
@@ -36,57 +36,3 @@ const makeAST = (firstData, secondData) => {
 };
 
 export default makeAST;
-
-
-
-
-// const makeAST = (firstData, secondData) => {
-//   const keysFirstData = _.keys(firstData);
-//   const keysSecondData = _.keys(secondData);
-//   const uniqueKeysOfTwoObjects = _.union(keysFirstData, keysSecondData);
-
-//   const tree = uniqueKeysOfTwoObjects.map((key) => {
-//     if (!_.has(secondData, key)) {
-//       return {
-//         name: key,
-//         status: 'deleted',
-//         firstValue: firstData[key],
-//       };
-//     }
-
-//     if (!_.has(firstData, key)) {
-//       return {
-//         name: key,
-//         status: 'added',
-//         secondValue: secondData[key],
-//       };
-//     }
-
-//     if (_.isEqual(firstData[key], secondData[key])) {
-//       return {
-//         name: key,
-//         status: 'unchanged',
-//         firstValue: firstData[key],
-//         secondValue: secondData[key],
-//       };
-//     }
-
-//     if (typeof firstData[key] === 'object' && typeof secondData[key] === 'object') {
-//       return {
-//         name: key,
-//         status: 'hasChildren',
-//         children: makeAST(firstData[key], secondData[key]),
-//       };
-//     }
-
-//     return {
-//       name: key,
-//       status: 'changed',
-//       firstValue: firstData[key],
-//       secondValue: secondData[key],
-//     };
-//   });
-//   return tree;
-// };
-
-// export default makeAST;
