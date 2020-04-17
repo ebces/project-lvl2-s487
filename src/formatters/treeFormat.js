af76f3c1bdb3291abe33dfab9e43b2f471e5faf3
ebces <ptashinsky@tut.by>
2020-04-17 16:07:54 +0300

    correct

--- src/formatters/treeFormat.js
+++ src/formatters/treeFormat.js
@@ -1,82 +1,45 @@
-const objectToString = (object, nestingLevel = 1) => {
-  const third = ' '.repeat(nestingLevel * 4 - 2);
+import { isObject } from 'lodash';
 
-  const twoSpaces = ' '.repeat(nestingLevel * 4 + 2);
+
+const objectToString = (object, nestingLevel = 1) => {
+  const indentStep = nestingLevel * 4;
+  const indentBeforeName = ' '.repeat(indentStep + 4);
+  const indentAfterName = ' '.repeat(indentStep);
   const values = Object.entries(object);
   const partsOfString = values.map((node) => {
     const [key, value] = node;
     if (typeof value !== 'object') {
-      return `${twoSpaces}${key}: ${value}`;
+      return `${indentBeforeName}${key}: ${value}`;
     }
-    return `${twoSpaces}${key}: {\n${objectToString(value, twoSpaces)}${twoSpaces}}`;
+    return `${indentBeforeName}${key}: ${objectToString(value, nestingLevel + 1)}`;
   }, []);
-  return `{\n${partsOfString.join('\n')}\n${third}}`;
+  return `{\n${partsOfString.join('\n')}\n${indentAfterName}}`;
 };
 
-const buildString = (node, level) => (typeof node === 'object' ? objectToString(node, level) : node);
+const buildString = (node, level) => (isObject(node) ? objectToString(node, level) : node);
 
 const render = (data, nestingLevel = 1) => {
-  const indent = ' ';
   const indentStep = nestingLevel * 4;
-  const twoSpace = indent.repeat(indentStep - 2);
-  const fourSpaces = indent.repeat(indentStep);
-  const third = ' '.repeat(indentStep - 4);
+  const indentBeforeName = ' '.repeat(indentStep - 2);
+  const indentAfterName = ' '.repeat(indentStep - 4);
+
   const result = data.map((node) => {
     switch (node.status) {
       case 'hasChildren':
-        return `${twoSpace}  ${node.name}: ${render(node.children, nestingLevel + 1)}`;
+        return `${indentBeforeName}  ${node.name}: ${render(node.children, nestingLevel + 1)}`;
       case 'added':
-        return `${twoSpace}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
+        return `${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
       case 'deleted':
-        return `${twoSpace}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
+        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
       case 'changed':
-        return `${twoSpace}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}\n${twoSpace}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
+        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}\n${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
       case 'unchanged':
-        return `${twoSpace}  ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
+        return `${indentBeforeName}  ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
       default:
         throw new Error(`Invalid status: ${node.status}`);
     }
   });
-  return `{\n${result.join('\n')}\n${third}}`;
+  return `{\n${result.join('\n')}\n${indentAfterName}}`;
 };
 
 export default render;
-
-// const objectToString = (object, spaces = '') => {
-//   const twoSpaces = `${spaces}    `;
-//   const values = Object.entries(object);
-//   const partsOfString = values.map((node) => {
-//     const [key, value] = node;
-//     if (typeof value !== 'object') {
-//       return `${twoSpaces}${key}: ${value}`;
-//     }
-//     return `${twoSpaces}${key}: {\n${objectToString(value, twoSpaces)}${twoSpaces}}`;
-//   }, []);
-//   return partsOfString.join('\n');
-// };
-
-// const buildString = (node, spaces) => (typeof node === 'object' ? `{\n${objectToString(node, spaces)}\n${spaces}}` : node);
-
-// const render = (data, spaces = '') => {
-//   const twoSpace = `${spaces}  `;
-//   const fourSpaces = `${spaces}    `;
-//   const result = data.map((node) => {
-//     switch (node.status) {
-//       case 'hasChildren':
-//         return `${twoSpace}  ${node.name}: ${render(node.children, fourSpaces)}`;
-//       case 'added':
-//         return `${twoSpace}+ ${node.name}: ${buildString(node.secondValue, fourSpaces)}`;
-//       case 'deleted':
-//         return `${twoSpace}- ${node.name}: ${buildString(node.firstValue, fourSpaces)}`;
-//       case 'changed':
-//         return `${twoSpace}- ${node.name}: ${buildString(node.firstValue, fourSpaces)}\n${twoSpace}+ ${node.name}: ${buildString(node.secondValue, fourSpaces)}`;
-//       case 'unchanged':
-//         return `${twoSpace}  ${node.name}: ${buildString(node.firstValue, fourSpaces)}`;
-//       default:
-//         throw new Error(`Invalid status: ${node.status}`);
-//     }
-//   });
-//   return `{\n${result.join('\n')}\n${spaces}}`;
-// };
-
-// export default render;
