9a62b7c2c47c047346b183e763b6cefa40832d8a
pavel <ptashinsky@tut.by>
2020-04-21 21:09:17 +0300

    correct

--- src/formatters/treeFormat.js
+++ src/formatters/treeFormat.js
@@ -1,8 +1,8 @@
 import { isObject } from 'lodash';
 
 
-const objectToString = (object, nestingLevel = 1) => {
-  const indentStep = nestingLevel * 4;
+const objectToString = (object, depth = 1) => {
+  const indentStep = depth * 4;
   const indentBeforeName = ' '.repeat(indentStep + 4);
   const indentAfterName = ' '.repeat(indentStep);
   const values = Object.entries(object);
@@ -11,30 +11,30 @@ const objectToString = (object, nestingLevel = 1) => {
     if (!isObject(value)) {
       return `${indentBeforeName}${key}: ${value}`;
     }
-    return `${indentBeforeName}${key}: ${objectToString(value, nestingLevel + 1)}`;
+    return `${indentBeforeName}${key}: ${objectToString(value, depth + 1)}`;
   }, []);
   return `{\n${partsOfString.join('\n')}\n${indentAfterName}}`;
 };
 
 const buildString = (node, level) => (isObject(node) ? objectToString(node, level) : node);
 
-const render = (data, nestingLevel = 1) => {
-  const indentStep = nestingLevel * 4;
+const render = (data, depth = 1) => {
+  const indentStep = depth * 4;
   const indentBeforeName = ' '.repeat(indentStep - 2);
   const indentAfterName = ' '.repeat(indentStep - 4);
 
   const result = data.map((node) => {
     switch (node.status) {
       case 'hasChildren':
-        return `${indentBeforeName}  ${node.name}: ${render(node.children, nestingLevel + 1)}`;
+        return `${indentBeforeName}  ${node.name}: ${render(node.children, depth + 1)}`;
       case 'added':
-        return `${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
+        return `${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, depth)}`;
       case 'deleted':
-        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
+        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, depth)}`;
       case 'changed':
-        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, nestingLevel)}\n${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, nestingLevel)}`;
+        return `${indentBeforeName}- ${node.name}: ${buildString(node.firstValue, depth)}\n${indentBeforeName}+ ${node.name}: ${buildString(node.secondValue, depth)}`;
       case 'unchanged':
-        return `${indentBeforeName}  ${node.name}: ${buildString(node.firstValue, nestingLevel)}`;
+        return `${indentBeforeName}  ${node.name}: ${buildString(node.firstValue, depth)}`;
       default:
         throw new Error(`Invalid status: ${node.status}`);
     }
